b593f684b50d98d1eb1a62ca53fd6814567464c6
Jason He <jasonhe233@hotmail.com>
2018-03-30 22:33:10 -0400

    Create test samplehandler that plays a single sample using Pyaudio

---
+++ src/SampleHandler.py
@@ -0,0 +1,30 @@
+import pyaudio
+import wave
+
+CHUNK = 1024
+
+wf = wave.open('../resources/test.wav', 'rb')
+
+# instantiate PyAudio
+p = pyaudio.PyAudio()
+
+# open stream
+stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                channels=wf.getnchannels(),
+                rate=wf.getframerate(),
+                output=True)
+
+# read data
+data = wf.readframes(CHUNK)
+
+# play stream
+while len(data) > 0:
+    stream.write(data)
+    data = wf.readframes(CHUNK)
+
+# stop stream
+stream.stop_stream()
+stream.close()
+
+# close PyAudio
+p.terminate()
